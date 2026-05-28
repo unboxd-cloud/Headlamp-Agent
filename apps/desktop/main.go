41f54e4d8e2dbb8599da2519c6d04dcd3887049c
Chinmay Panda <chinmay@openautonomyx.com>
2026-05-28 16:26:07 -0700

    Add Wails desktop main entrypoint

---
+++ apps/desktop/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"embed"
+
+	"github.com/wailsapp/wails/v2"
+	"github.com/wailsapp/wails/v2/pkg/options"
+	"github.com/wailsapp/wails/v2/pkg/options/assetserver"
+)
+
+//go:embed all:frontend/dist
+var assets embed.FS
+
+func main() {
+	app := NewApp()
+
+	err := wails.Run(&options.App{
+		Title:  "Headlamp Agent",
+		Width:  1100,
+		Height: 760,
+		AssetServer: &assetserver.Options{
+			Assets: assets,
+		},
+		OnStartup: app.startup,
+		Bind: []interface{}{
+			app,
+		},
+	})
+
+	if err != nil {
+		println("Error:", err.Error())
+	}
+}
